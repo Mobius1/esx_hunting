01ea8cadcd6bf98ebb6dfdd16cfa4e2d1009c5cd
Mobius1 <mobius1@gmx.com>
2020-03-25 18:52:24 +0000

    Add pull requests

--- client/main.lua
+++ client/main.lua
@@ -12,6 +12,23 @@ local Keys = {
 
 ESX                             = nil
 
+local Blips = {
+	{ name="Hunting", id=141,x=-769.23773193359, y=5595.6215820313, z=33.48571395874},
+	{ name="Hunting Seller", id=141,x=-969.16375732422, y=-2107.9033203125, z=31.475671768188},
+}
+
+Citizen.CreateThread(function()
+	for _, item in pairs(Blips) do
+		item.blip = AddBlipForCoord(item.x, item.y, item.z)
+		SetBlipSprite(item.blip, item.id)
+		SetBlipColour(item.blip, 1)
+		SetBlipAsShortRange(item.blip, true)
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(item.name)
+		EndTextCommandSetBlipName(item.blip)	
+	end
+end)
+
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
--- server/main.lua
+++ server/main.lua
@@ -6,12 +6,12 @@ RegisterServerEvent('esx-qalle-hunting:reward')
 AddEventHandler('esx-qalle-hunting:reward', function(Weight)
     local xPlayer = ESX.GetPlayerFromId(source)
 
-    if Weight >= 1 then
-        xPlayer.addInventoryItem('meat', 1)
+    if Weight >= 15 then
+        xPlayer.addInventoryItem('meat', 3)
     elseif Weight >= 9 then
         xPlayer.addInventoryItem('meat', 2)
-    elseif Weight >= 15 then
-        xPlayer.addInventoryItem('meat', 3)
+    elseif Weight >= 1 then
+        xPlayer.addInventoryItem('meat', 1)
     end
 
     xPlayer.addInventoryItem('leather', math.random(1, 4))
